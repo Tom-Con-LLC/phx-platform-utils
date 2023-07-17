3f0586e98ab5e4ac93edaac224d8a01049a55a6c
JD Crone <jd@deliverynative.com>
2023-07-17 10:24:13 -0600

    change to claims function

--- lib/utils/token.ex
+++ lib/utils/token.ex
@@ -2,21 +2,11 @@ defmodule PhxPlatformUtils.Utils.Token do
   alias PhxPlatformUtils.Errors
 
   def claims(conn) do
-    base = conn.assigns[:user] || %{}
-
-    override_email = base |> Map.get(:"https://deliverynative.com/email", nil)
-
-    if override_email != nil do
-      base
-      |> Map.merge(%{email: override_email})
-      |> Map.delete(:"https://deliverynative.com/email")
-    else
-      base
-    end
+    conn.assigns[:user] || %{}
   end
 
   def get_matching_permissions(conn, allowed_permissions) do
-    allowed = allowed_permissions |> MapSet.new() |> MapSet.put("sys:admin")
+    allowed = allowed_permissions |> MapSet.new()
 
     conn
     |> claims()
