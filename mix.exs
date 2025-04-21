f11d34e35365792f97ea50d51cafb85776749332
sherab <sherabyanks@gmail.com>
2025-04-21 14:57:04 +0500

    feat: updates packages

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhxPlatformUtils.MixProject do
     [
       app: :phx_platform_utils,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,19 +21,19 @@ defmodule PhxPlatformUtils.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:castore, ">= 0.0.0"},
-      {:certifi, "~> 2.8"},
+      {:castore, "~> 1.0.12"},
+      {:certifi, "~> 2.14.0"},
       {:ecto_sql, "~> 3.10"},
-      {:faker, "~> 0.17"},
+      {:faker, "~> 0.18"},
       {:gen_rmq, "~> 4.0"},
-      {:httpoison, "~> 1.8"},
-      {:inflex, "~> 2.0.0"},
-      {:jason, "~> 1.4"},
+      {:httpoison, "~> 2.2.3"},
+      {:inflex, "~> 2.1.0"},
+      {:jason, "~> 1.4.4"},
       {:joi, "~> 0.2.1"},
-      {:logger_json, "~> 5.1"},
-      {:phoenix, "~> 1.7.7"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:redix, "~> 1.1"}
+      {:logger_json, "~> 7.0.0"},
+      {:phoenix, "~> 1.7.21"},
+      {:phoenix_ecto, "~> 4.6.3"},
+      {:redix, "~> 1.5.2"}
     ]
   end
 end
